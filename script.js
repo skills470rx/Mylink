49b53fd2248db6965d328cca5b72c17abd9cffcc
𝐚𝐥𝐩𝐡𝐚 <alpha470rx@gmail.com>
2026-07-11 12:02:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+const myLinks = [
+    {
+        title: "Shop 🥰",
+        url: "https://productink.netlify.app/",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "Telegram: Play @alpha470rx",
+        url: "https://t.me/alpha470rx?direct",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "Telegram: Channel @alpha470rx",
+        url: "https://t.me/alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "Telegram Group @alpha473rx",
+        url: "https://t.me/alpha473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "YouTube @alpha470rx",
+        url: "https://www.youtube.com/@alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "YouTube @alpha471rx",
+        url: "https://www.youtube.com/@alpha471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "TikTok @alpha472rx",
+        url: "https://tiktok.com/@alpha472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96ps2de878c73b63i40/6a09a41513340d3d8536908701532d63.webp"
+    },
+    {
+        title: "GitHub @alpha470rx",
+        url: "https://github.com/alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "TikTok @alpha471rx",
+        url: "https://tiktok.com/@alpha471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "Facebook @alpha472rx",
+        url: "https://www.facebook.com/share/1CqYcMraUu/",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96ps2de878c73b63i40/6a09a41513340d3d8536908701532d63.webp"
+    },
+    {
+        title: "Facebook @alpha470rx",
+        url: "https://www.facebook.com/share/18nPiGktHk/",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "WhatsApp",
+        url: "https://whatsapp.com/channel/0029Vb7m4ZR6buMNGT72E41E",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "Instagram @alpha470rx",
+        url: "https://www.instagram.com/alpha470rx?igsh=am50c3R0MmNkYzll",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "Threads @alpha470rx",
+        url: "https://www.threads.com/@alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "X @alpha470rx",
+        url: "https://x.com/alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "Facebook @alpha471rx",
+        url: "https://www.facebook.com/share/1BWjjSgFTm/",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "Manus",
+        url: "https://manus.im/invitation/PRHTCPUJUP5KHQ?utm_source=invitation&utm_medium=social&utm_campaign=system_share",
+        image: "https://hdrobots.com/wp-content/uploads/2025/03/manus-ai-logo-transparent.webp"
+    },
+    {
+        title: "Instagram @alpha471rx",
+        url: "https://www.instagram.com/alpha471rx?igsh=d2hhMmh1bGRpc3Zm",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "pixai @alpha470rx",
+        url: "https://pixai.art/referral?refCode=FHT3XDAR&utm_source=referral",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "Instagram @alpha473rx",
+        url: "https://www.instagram.com/alpha473rx?igsh=MTRkOHZud2tnN3BtdQ==",
+        image: "https://image.cdn2.seaart.me/2026-07-06/d95gntte878c73ah22ag/cdc6d9a3f4be7efb60bbe70e53031c28.webp"
+    },
+    {
+        title: "TikTok @alpha470rx",
+        url: "https://tiktok.com/@alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "Threads: @alpha473rx",
+        url: "https://www.threads.com/@alpha473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97gdcte878c738qc72g/ed9dd8a82ba799a27fd5a4996665649e.webp"
+    },
+    {
+        title: "Threads: @alpha471rx",
+        url: "https://www.threads.com/@alpha473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-05/d94rf2te878c73et5o40/802280765f2e16ee5f0ab918c3c77a25.webp"
+    },
+    {
+        title: "seaart ai: alpha",
+        url: "https://www.seaart.ai/th/user/c3ce28e041527cad5428d59d317c60e7?u_code=4XUHJZA9",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/1f9d5121bfc71b276cdd1a6854b8b6e3.webp"
+    },
+    {
+        title: "seaart ai: power",
+        url: "https://www.seaart.ai/th/user/b5bae092d86cc2ddbb3b8c22e60dff01?u_code=XSVGDKAX",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/3de5afd4f8b64e0b3c30b097eaca9993.webp"
+    },
+    {
+        title: "seaart ai skills",
+        url: "https://www.seaart.ai/th/user/a59187a509d648fb02899c8b0f8b40d6?u_code=Z5JC9JFY",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+];
+
+
+function renderLinks() {
+    const container = document.getElementById('links-container');
+    container.innerHTML = ''; //
+
+    myLinks.forEach(link => {
+        //
+        const linkElement = document.createElement('a');
+        linkElement.href = link.url;
+        linkElement.target = "_blank"; //
+        linkElement.classList.add('link-card');
+
+        const imgElement = document.createElement('img');
+        imgElement.src = link.image;
+        imgElement.alt = link.title;
+        imgElement.classList.add('link-thumb');
+
+        const titleElement = document.createElement('span');
+        titleElement.classList.add('link-title');
+        titleElement.textContent = link.title;
+
+        linkElement.appendChild(imgElement);
+        linkElement.appendChild(titleElement);
+        container.appendChild(linkElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderLinks);
