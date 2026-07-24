6a8f240eb1e3936215d6600f95db4b52fd144d27
𝐚𝐥𝐩𝐡𝐚 <alpha470rx@gmail.com>
2026-07-24 13:58:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,221 @@
+const myLinks = [
+    {
+        title: "Shop 🥰",
+        url: "https://productink.netlify.app/",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/143cbcd707d7ffe26e491a1a3d49527e.webp"
+    },
+    {
+        title: "Telegram: Play @alpha470rx",
+        url: "https://t.me/skills470rx?direct",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "Telegram: Channel @alpha470rx",
+        url: "https://t.me/skills470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "Telegram Group @skills472rx",
+        url: "https://t.me/skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/8ffafa5ade6b109bcfab7f84a2121efb.webp"
+    },
+    {
+        title: "GitHub @skills470rx",
+        url: "https://github.com/skills470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "GitHub @alpha471rx",
+        url: "https://github.com/skills471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "GitHub @skills472rx",
+        url: "https://github.com/skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "GitHub @skills473rx",
+        url: "https://github.com/skills473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "YouTube @skills470rx",
+        url: "https://www.youtube.com/@skills470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "YouTube @skills471rx",
+        url: "https://www.youtube.com/@skills471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "YouTube @skills472rx",
+        url: "https://www.youtube.com/@skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "YouTube @skills473rx",
+        url: "https://www.youtube.com/@skills473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "TikTok @skills470rx",
+        url: "https://tiktok.com/@skills470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "TikTok @skills471rx",
+        url: "https://tiktok.com/@skills471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "TikTok @skills472rx",
+        url: "https://tiktok.com/@skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "TikTok @skills473rx",
+        url: "https://tiktok.com/@skills473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "Facebook @skills470rx",
+        url: "https://www.facebook.com/share/1CqYcMraUu/",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "Facebook @skills471rx",
+        url: "https://www.facebook.com/share/18nPiGktHk/",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "Facebook @skills472rx",
+        url: "https://www.facebook.com/share/1BWjjSgFTm/",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "Facebook @skills473rx",
+        url: "https://www.facebook.com/share/18xMRtZFkN/",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "WhatsApp",
+        url: "https://whatsapp.com/channel/0029Vb7m4ZR6buMNGT72E41E",
+        image: "https://image.cdn2.seaart.me/2026-07-07/d966slde878c738ugeeg/dd972cad816db614ee78f94669156d96.webp"
+    },
+    {
+        title: "Instagram @skills70rx",
+        url: "https://www.instagram.com/skills470rx?igsh=am50c3R0MmNkYzll",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "Instagram @skills471rx",
+        url: "https://www.instagram.com/skills471rx?igsh=MTdkamk4NGVjd2lhaw==",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "Instagram @skills472rx",
+        url: "https://www.instagram.com/skills472rx?igsh=MTRkOHZud2tnN3BtdQ==",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "Instagram @skills473rx",
+        url: "https://www.instagram.com/skills473rx?igsh=MXdpeGk2emJzMjljag==",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "Threads: @skills470rx",
+        url: "https://www.threads.com/@skills470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "Threads @skills471rx",
+        url: "https://www.threads.com/@skills471rx",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "Threads: @skills472rx",
+        url: "https://www.threads.com/@skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "Threads: @skills473rx",
+        url: "https://www.threads.com/@skills473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "X @skills470rx",
+        url: "https://x.com/alpha470rx",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "X @skills471rx",
+        url: "https://x.com/antiban392895",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.webp"
+    },
+    {
+        title: "X @skills472rx",
+        url: "https://x.com/skills472rx",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "X @skills473rx",
+        url: "https://x.com/skills473rx",
+        image: "https://image.cdn2.seaart.me/2026-07-23/d9gts0de878c73fmjjhg/bd9db4a6cc50c0f6862c4b5082e2a0e5.webp"
+    },
+    {
+        title: "Manus",
+        url: "https://manus.im/invitation/PRHTCPUJUP5KHQ?utm_source=invitation&utm_medium=social&utm_campaign=system_share",
+        image: "https://hdrobots.com/wp-content/uploads/2025/03/manus-ai-logo-transparent.webp"
+    },
+    {
+        title: "seaart ai: alpha",
+        url: "https://www.seaart.ai/th/user/c3ce28e041527cad5428d59d317c60e7?u_code=4XUHJZA9",
+        image: "https://image.cdn2.seaart.me/2026-07-10/d9865kde878c739tkt2g/d5a4a857ef1e7cb9e10a983aed4da264.webp"
+    },
+    {
+        title: "seaart ai: Xiaowen",
+        url: "https://www.seaart.ai/th/user/b5bae092d86cc2ddbb3b8c22e60dff01?u_code=XSVGDKAX",
+        image: "https://image.cdn2.seaart.me/2026-07-09/d97g6v5e878c73ce51f0/0e52b1614bfc924cb2664d2f22680d07.webp"
+    },
+    {
+        title: "seaart ai 473",
+        url: "https://www.seaart.ai/user/f9f90015de4817345f0eb6ef771ad5c9?u_code=8LRF4RD2",
+        image: "https://image.cdn2.seaart.me/2026-07-24/d9hb5d5e878c73cjntn0/f73c14b9c75b880861ea80e4f57ca6b7.webp"
+    },
+    {
+        title: "seaart ai skills",
+        url: "https://www.seaart.ai/th/user/a59187a509d648fb02899c8b0f8b40d6?u_code=Z5JC9JFY",
+        image: "https://image.cdn2.seaart.me/2026-07-08/d96phtle878c73fhfnh0/cfb2660642cd331dd560ebebe9e1b7df.web"
+    },
+];
+
+
+function renderLinks() {
+    const container = document.getElementById('links-container');
+    container.innerHTML = ''; //
+
+    myLinks.forEach(link => {
+        //
+        const linkElement = document.createElement('a');
+        linkElement.href = link.url;
+        linkElement.target = "_blank"; //
+        linkElement.classList.add('link-card');
+
+        const imgElement = document.createElement('img');
+        imgElement.src = link.image;
+        imgElement.alt = link.title;
+        imgElement.classList.add('link-thumb');
+
+        const titleElement = document.createElement('span');
+        titleElement.classList.add('link-title');
+        titleElement.textContent = link.title;
+
+        linkElement.appendChild(imgElement);
+        linkElement.appendChild(titleElement);
+        container.appendChild(linkElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderLinks);
